95e3e6b7e5ca55dea8fc52224fd1ac7badfd4044
elingaldo <arnaldo.castilla@gmail.com>
2024-03-17 18:18:38 -0500

    - repositories - razo component generic - sweetalert2

--- Orders/Orders.Backend/Controllers/CountriesController.cs
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -18,7 +18,7 @@ namespace Orders.Backend.Controllers
 
         [HttpGet]
         public async Task<IActionResult> GetAsync() {
-            return Ok(await _context.Countries.ToListAsync());
+            return Ok(await _context.Countries.AsNoTracking().ToListAsync());
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetAsync(int id) {
