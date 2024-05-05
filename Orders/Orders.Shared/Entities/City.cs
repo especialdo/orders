98f602082f40b809ebb1e3323b52efb45326872f
ARNALDO RAFAEL CASTILLA FLOREZ <arnaldocastilla1134881@correo.itm.edu.co>
2024-05-04 20:26:50 -0500

    add userentity

--- Orders/Orders.Shared/Entities/City.cs
+++ Orders/Orders.Shared/Entities/City.cs
@@ -18,6 +18,7 @@ namespace Orders.Shared.Entities
         public string Name { get; set; } = null!;
         public int StateId { get; set; }
         public State? State { get; set; }
+        public ICollection<User> Users { get; set; }
     }
 }
 
